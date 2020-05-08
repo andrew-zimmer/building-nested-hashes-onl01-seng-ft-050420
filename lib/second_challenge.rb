8211cc2e8f266b5530f47fa661eb98262c1e630e
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-08 13:43:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      partiarch: "",
-      matriarch: "",
-      hero: "",
-      hero_friends: ""
+      partiarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       }, 
     :capulet => {
-       partiarch: "",
-      matriarch: "",
-      hero: "",
-      hero_friends: ""
+       partiarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       }}
 
   
